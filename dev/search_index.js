753f3bfbfec998e0cae84fe6d99b03b692f76206
Documenter.jl <documenter@juliadocs.github.io>
2023-12-27 09:20:18 +0000

    build based on 2eb772a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = AdaptiveSampling","category":"page"},{"location":"#AdaptiveSampling","page":"Home","title":"AdaptiveSampling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for AdaptiveSampling.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [AdaptiveSampling]","category":"page"}]
+}
