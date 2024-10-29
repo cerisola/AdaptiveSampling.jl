1517e3080713f6c8245a490241952b31076b0fbd
Federico Cerisola <federico@cerisola.net>
2024-10-29 07:22:43 +0000

    Removed commented out old code

--- src/geometry.jl
+++ src/geometry.jl
@@ -1,5 +1,3 @@
 @inline midpoint(x) = (first(x) + last(x))/2
 
 @inline area(x, y) = 0.5*abs(x[1]*(y[2] - y[3]) + x[2]*(y[3] - y[1]) + x[3]*(y[1] - y[2]))
-
-# centroid(t::Triangle{T}) where {T<:Real} = (t.x[1] + t.x[2] + t.x[3])/3
