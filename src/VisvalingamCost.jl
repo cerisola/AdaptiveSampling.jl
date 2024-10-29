166ff956444bf6fa1a81eacb6f70242690f08490
Federico Cerisola <federico@cerisola.net>
2024-10-29 07:21:33 +0000

    Fix sign of Visvalingam norm

--- src/VisvalingamCost.jl
+++ src/VisvalingamCost.jl
@@ -22,7 +22,7 @@ struct VisvalingamCost <: AbstractCost{3}
 
     VisvalingamCost(norm) = new(Float64(norm))
 
-    VisvalingamCost(a, b, fa, fb) = VisvalingamCost((b - a)*(fb - fa)/2)
+    VisvalingamCost(a, b, fa, fb) = VisvalingamCost(abs((b - a)*(fb - fa))/2)
 end
 
 (c::VisvalingamCost)(x, y) = sqrt(area(x, y)/c.norm)
